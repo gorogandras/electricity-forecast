95795c5e0e03db2dafe6293fcefa5bcee07a3194
András GÖRÖG <gorog.andras.gabor@gmail.com>
2024-02-10 11:39:11 +0100

    change social media links

--- app.py
+++ app.py
@@ -21,12 +21,12 @@ def index():
     if request.method == 'POST':
         try:
             moment_to_pred = [[str(request.form['moment_to_pred'])]]
-
             obj = PredictionPipeline()
             datetime_df = obj.create_datetime_df(moment_to_pred)
             predict = obj.predict(datetime_df)
-
+            
             return render_template('results.html', prediction = round(predict[0]))
+        
         except Exception as e:
             print('The Exception message is: ', e)
             return 'something is wrong'
